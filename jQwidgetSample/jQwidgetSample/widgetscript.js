1895941a3fd8be17b00b468bf8abbe8ad6e2317d
Avinash52 <avinashj.dream@gmail.com>
2015-06-15 16:52:08 +0530

    another commit

--- jQwidgetSample/jQwidgetSample/widgetscript.js
+++ jQwidgetSample/jQwidgetSample/widgetscript.js
@@ -1,7 +1,7 @@
 ﻿$(document).ready(function () {
     // Create jqxTree
     $('#jqxTree').jqxTree({ height: '300px', width: '300px' });
-    $('#jqxTree').bind('select', function (event) {
+       $('#jqxTree').bind('select', function (event) {
         var htmlElement = event.args.element;
         var item = $('#jqxTree').jqxTree('getItem', htmlElement);
         alert(item.label);
