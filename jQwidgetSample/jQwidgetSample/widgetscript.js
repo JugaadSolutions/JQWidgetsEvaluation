6b95d5e7af59dbd25251da32d20bd55b6fd2acda
Avinash52 <avinashj.dream@gmail.com>
2015-06-15 14:32:11 +0530

    sample commit

---
+++ jQwidgetSample/jQwidgetSample/widgetscript.js
@@ -0,0 +1,9 @@
+﻿$(document).ready(function () {
+    // Create jqxTree
+    $('#jqxTree').jqxTree({ height: '300px', width: '300px' });
+    $('#jqxTree').bind('select', function (event) {
+        var htmlElement = event.args.element;
+        var item = $('#jqxTree').jqxTree('getItem', htmlElement);
+        alert(item.label);
+    });
+});
