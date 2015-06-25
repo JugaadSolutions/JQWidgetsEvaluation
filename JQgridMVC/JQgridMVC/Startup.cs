7a49f8b0bda509e189558194aefe28cd8c9657b6
Avinash52 <avinashj.dream@gmail.com>
2015-06-25 18:36:57 +0530

    JQgridMVC commit

---
+++ JQgridMVC/JQgridMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(JQgridMVC.Startup))]
+namespace JQgridMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
