0a05660427a1c895da3bd1cb4cac87ca6c2827f6
Avinash52 <avinashj.dream@gmail.com>
2015-07-10 19:19:26 +0530

    image fix

---
+++ jQwidgetSample/jQwidgetSample/Treescript.js
@@ -0,0 +1,24 @@
+﻿$(document).ready(function () {
+    var url="jsdata.txt";
+    var source =
+                {
+                    datatype: "json",
+                    datafields: [
+                        { name: 'id' },
+                        { name: 'parentid' },
+                        { name: 'text' },
+                        { name: 'value' }
+                    ],
+                    id: 'id',
+                    url:url
+                };
+// create data adapter.
+var dataAdapter = new $.jqx.dataAdapter(source);
+// perform Data Binding.
+dataAdapter.dataBind();
+// get the tree items. The first parameter is the item's id. The second parameter is the parent item's id. The 'items' parameter represents 
+// the sub items collection name. Each jqxTree item has a 'label' property, but in the JSON data, we have a 'text' field. The last parameter 
+// specifies the mapping between the 'text' and 'label' fields.  
+var records = dataAdapter.getRecordsHierarchy('id', 'parentid', 'items', [{ name: 'text', map: 'label'}]);
+$('#jqxWidget').jqxTree({ source: records, width: '300px'});
+});
